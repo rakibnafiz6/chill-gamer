dff8e994ad49c9ee96df0efa78c14c78269d442b
nafiz rakib <rakibnafiz6@gmail.com>
2024-12-06 19:18:53 +0600

    added the delete button functionality

--- src/pages/MyReviews.jsx
+++ src/pages/MyReviews.jsx
@@ -1,10 +1,47 @@
 import { useLoaderData } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 const MyReviews = () => {
     const reviews = useLoaderData();
+    const [users, setUsers] = useState(reviews);
 
 
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/gamers/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                       if(data.deletedCount){
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                          });
+
+                        const remainingReviews = users.filter(user => user._id !== id);
+                        setUsers(remainingReviews);
+                       }
+
+            })
+            }
+        });
+
+    }
 
 
 
@@ -28,21 +65,23 @@ const MyReviews = () => {
                     </thead>
                     <tbody>
                         {
-                            reviews.map((review, idx)=><tr>
-                            <th>{idx + 1}</th>
-                            <td>{review.name}</td>
-                            <td>{review.rating}</td>
-                            <td>{review.genres}</td>
-                            <td><div>
-                                <button className="btn mb-2 lg:mr-2">Update</button>
-                                <button className="btn">Delete</button>
+                            reviews.map((review, idx) => <tr key={idx}>
+                                <th>{idx + 1}</th>
+                                <td>{review.name}</td>
+                                <td>{review.rating}</td>
+                                <td>{review.genres}</td>
+                                <td><div>
+                                    <button className="btn mb-2 lg:mr-2 btn-accent">Update</button>
+                                    <button
+                                        onClick={()=> handleDelete(review._id)}
+                                        className="btn btn-error">Delete</button>
                                 </div></td>
-                        </tr>)
+                            </tr>)
                         }
-                       
+
                     </tbody>
                 </table>
-               
+
             </div>
 
         </div>
